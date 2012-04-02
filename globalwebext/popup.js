01cb189c744c5cabdeb973b7b4104db141735916
Yiannis Yiakoumis <yiannisy@stanford.edu>
2012-04-02 13:14:03 -0700

    Send correct country request.

--- globalwebext/popup.js
+++ globalwebext/popup.js
@@ -7,16 +7,19 @@ jQuery.getJSON("http://192.168.1.217:8000/gw/get_countries",function(data){
 			$(document.createElement("img"))
 				.attr({src:data[i].img_url,title:data[i].code})
 				.appendTo(document.body)
-				.click(function(){
-						var country_url = "http://192.168.1.217:8000/gw/get_country?code=GR";
-						jQuery.getJSON(country_url, function(data){
+				.click({details:data[i]},function(event){
+						var details = event.data.details;
+						console.log("Fetching proxy details for " + details.name);
+						var country_url = "http://192.168.1.217:8000/gw/get_country?code=" + event.data.details.code;
+						jQuery.getJSON(country_url, function(proxy_details){
+								console.log("Setting proxy to " + proxy_details.proxy_url + ":" + proxy_details.proxy_port);
 								var config = {
 									mode: "fixed_servers",
 									rules: {
 										singleProxy: {
 											scheme: "http",
-											host: data.proxy_url,
-											port:data.proxy_port
+											host: proxy_details.proxy_url,
+											port: proxy_details.proxy_port
 										},
 									}
 								};
