4c6403c249a707f754a8bfb9442af04cfabd73d7
Yiannis Yiakoumis <yiannisy@stanford.edu>
2012-04-02 19:22:28 -0700

    Fallback to system proxy when home-icon is clicked.

--- globalwebext/popup.js
+++ globalwebext/popup.js
@@ -8,34 +8,43 @@ var lia_url = "http://192.168.1.217:8000";
 
 var countries_url = lia_url + "/gw/get_countries";
 jQuery.getJSON(countries_url,function(data){
-		for (var i = 0; i <= 2;i++){
+		for (var i = 0; i <= data.length; i++){
 			$(document.createElement("img"))
 				.attr({src:data[i].img_url,title:data[i].code})
 				.appendTo(document.body)
 				.click({details:data[i]},function(event){
 						var details = event.data.details;
-						console.log("Fetching proxy details for " + details.name);
-						var country_url = lia_url + "/gw/get_country?code=" + details.code;
-						jQuery.getJSON(country_url, function(proxy_details){
-								console.log("Setting proxy to " + proxy_details.proxy_url + ":" + proxy_details.proxy_port);
-								var config = {
-									mode: "fixed_servers",
-									rules: {
-										singleProxy: {
-											scheme: "http",
-											host: proxy_details.proxy_url,
-											port: proxy_details.proxy_port
-										},
-										bypassList: [lia_server]
-									}
-								};
-								chrome.proxy.settings.set(
-														  {value: config, scope: 'regular'},
-														  function() {});
-							});						
-						chrome.browserAction.setBadgeBackgroundColor({color:[0,0,128,255]});
-						chrome.browserAction.setBadgeText({text:details.code});
-
+						if (details.code == "XX"){
+							// Deactivate proxy
+							chrome.browserAction.setBadgeText({text:String("")});
+							chrome.proxy.settings.set({value:{mode:"system"}});
+							console.log("Deactivating proxy..");
+						}
+						else {
+							console.log("Fetching proxy details for " + details.name);
+							var country_url = lia_url + "/gw/get_country?code=" + details.code;
+							jQuery.getJSON(country_url, function(proxy_details){
+									console.log("Setting proxy to " + proxy_details.proxy_url + ":" + proxy_details.proxy_port);
+									var config = {
+										mode: "fixed_servers",
+										rules: {
+											singleProxy: {
+												scheme: "http",
+												host: proxy_details.proxy_url,
+												port: proxy_details.proxy_port
+											},
+											bypassList: [lia_server]
+										}
+									};
+									chrome.proxy.settings.set(
+															  {value: config, scope: 'regular'},
+															  function() {});
+								});						
+							chrome.browserAction.setBadgeBackgroundColor({color:[0,0,128,255]});
+							chrome.browserAction.setBadgeText({text:details.code});
+						}
+						
+						
 					})
 				}
 	});
--- gw/gwapi/views.py
+++ gw/gwapi/views.py
@@ -12,10 +12,12 @@ def get_proxy_url(code):
     
 def get_countries(req):
     if req.method == "GET":
-        return HttpResponse(json.dumps([{"code":"GR","name":"Greece","img_url":"http://yuba.stanford.edu/~yiannis/greece.png"},
-                                        {"code":"US","name":"United States","img_url":"http://yuba.stanford.edu/~yiannis/us.png"},
-                                        {"code":"UK","name":"United Kingdom","img_url":"http://yuba.stanford.edu/~yiannis/uk.png"},
-                                        ]))
+        return HttpResponse(json.dumps([
+            {"code":"XX","name":"Deactivate","img_url":"icon.png"},
+            {"code":"GR","name":"Greece","img_url":"http://yuba.stanford.edu/~yiannis/greece.png"},
+            {"code":"US","name":"United States","img_url":"http://yuba.stanford.edu/~yiannis/us.png"},
+            {"code":"UK","name":"United Kingdom","img_url":"http://yuba.stanford.edu/~yiannis/uk.png"},
+            ]))
 
 def get_country(req):
     if req.method == "GET":
@@ -23,5 +25,4 @@ def get_country(req):
         proxy_url = get_proxy_url(code)
         url,port = proxy_url.split(':')
         port = int(port)
-        #return HttpResponse("ok")
         return HttpResponse(json.dumps({"code":code,"proxy_url":url,"proxy_port":port}))
